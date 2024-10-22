5a0fb13eda1aa4e444b6406e0f8a7b084432feea
Robin Soni <sonirobin369@gmail.com>
2024-10-22 11:11:09 +0530

    watch service included (not protected.)

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import UploadPage from "./pages/uploadPage";
 import AuthPage from "./pages/auth";
+import AppHome from "./pages/appHome";
 
 export default function Home() {
   return ( 
     <>
-      <div><UploadPage/></div>
-     {/*  <div><AuthPage /></div> */}
+      {<div><UploadPage/></div>}
+      <div> test ytb</div>
+      <div><AppHome /></div>
+     {/* <div><AuthPage /></div> */}
     </>
   );
 }
